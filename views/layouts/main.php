6332647bc43f8e904a6a758bb955c0b7cebf58c2
OleksandrSGr <oleksandr.sgr@gmail.com>
2017-05-26 17:38:45 +0300

    add header

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -23,38 +23,83 @@ AppAsset::register($this);
 </head>
 <body>
 <?php $this->beginBody() ?>
+<div class="container">
 
-<div class="wrap">
+    <div class="container">
+        <div class="container row">
+            <div class="col-xs-6">
+                <div class="logo-text">
+                    <t id="logo-text-1">Best-PHP-</t><t id="logo-text-2">Developers</t><t id="logo-text-3">.com</t>
+                </div>
+                <div id="logo-rectangle-1">
+                    <p>QUALITY PHP DEVELOPMENT & PHP DEVELOPERS FOR HIRE</p>
+                </div>
+                <div id="logo-rectangle-2">
+                    More than <strong style="color: #f2b824">80</strong> PHP developers.
+                </div>
+            </div>
+            <div class="col-xs-6">
+                <table class="inform-table">
+                    <tr>
+                        <td>
+                            <div class="social-icon">
+                                <a href="#"><img src="img/vk.png"></a>
+                                <a href="#"><img src="img/gp.png"></a>
+                                <a href="#"><img src="img/fb.png"></a>
+                                <a href="#"><img src="img/tw.png"></a>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div class="social-icon">
+                                <t class="ph-num">+1-347-630-0528</t>
+                                <t class="ph-num">+49-302-5555-5704</t>
+                                <t class="ph-num">+38-094-711-6778</t>
+                                <img src="img/ph.png">
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div class="social-icon">
+                                <t class="ph-num">Best.Developers</t>
+                                <img src="img/sk.png">
+                            </div>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+
+
+
     <?php
     NavBar::begin([
-        'brandLabel' => 'My Company',
-        'brandUrl' => Yii::$app->homeUrl,
         'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
+            'class' => 'navbar navbar-default',
         ],
     ]);
     echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
+        'options' => ['class' => 'navbar-nav'],
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
-            Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
-            ) : (
-                '<li>'
-                . Html::beginForm(['/site/logout'], 'post')
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
-                    ['class' => 'btn btn-link logout']
-                )
-                . Html::endForm()
-                . '</li>'
-            )
+            ['label' => 'PHP Development', 'url' => ['/site/about']],
+            ['label' => 'Hire PHP Developers', 'url' => ['/site/about']],
+            ['label' => 'Dedicated PHP Team', 'url' => ['/site/about']],
+            ['label' => 'Prices', 'url' => ['/site/about']],
+            ['label' => 'Projects', 'url' => ['/site/about']],
+            ['label' => 'Contacts', 'url' => ['/site/contact']],
+            ['label' => 'Sitemap', 'url' => ['/site/contact']],
         ],
     ]);
     NavBar::end();
     ?>
+</div>
+<div class="wrap">
+
+
 
     <div class="container">
         <?= Breadcrumbs::widget([
