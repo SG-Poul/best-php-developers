6fac0e9e6b234a882f61b27b7bf2e19180021db8
SG-Poul <sg.pavlop@gmail.com>
2017-06-08 16:23:33 +0300

    improving header adding links to social networks and interactive skype/phone numbers

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -34,9 +34,7 @@ $urlArray = explode('/', Yii::$app->request->url);
         <div class="row">
             <div class="col-md-6">
                 <div class="logo-text">
-                    <t id="logo-text-1">Best-PHP-</t>
-                    <t id="logo-text-2">Developers</t>
-                    <t id="logo-text-3">.com</t>
+                    <a href="/"><img id="logoImage" src="/img/logo.png"></a>
                 </div>
                 <div id="logo-rectangle-1">
                     <p>QUALITY PHP DEVELOPMENT & PHP DEVELOPERS FOR HIRE</p>
@@ -47,22 +45,22 @@ $urlArray = explode('/', Yii::$app->request->url);
             </div>
             <div class="col-md-6">
                 <div class="social-icon">
-                    <a href="#"><img src="/img/vk.png"></a>
-                    <a href="#"><img src="/img/gp.png"></a>
-                    <a href="#"><img src="/img/fb.png"></a>
-                    <a href="#"><img src="/img/tw.png"></a>
+                    <a href="http://vk.com/sg_it_company"><img src="/img/vk.png"></a>
+                    <a href="https://plus.google.com/u/0/b/106367725985310588511/dashboard/overview"><img src="/img/gp.png"></a>
+                    <a href="https://www.facebook.com/Soft-Group/"><img src="/img/fb.png"></a>
+                    <a href="https://twitter.com/SoftGroupCorp"><img src="/img/tw.png"></a>
                 </div>
 
                 <div class="social-number">
-                    <t class="ph-num">+1-347-630-0528</t>
-                    <t class="ph-num">+49-302-5555-5704</t>
-                    <t class="ph-num">+38-094-711-6778</t>
-                    <img src="/img/ph.png">
+                    <t class="ph-num"><a href="tel:+13476300528">+1-347-630-0528</a></t>
+                    <t class="ph-num"><a href="tel:+4930255555704">+49-302-5555-5704</a></t>
+                    <t class="ph-num"><a href="tel:+380947116778">+38-094-711-6778</a></t>
+                    <a href="tel:+13476300528"><img src="/img/ph.png"></a>
                 </div>
 
                 <div class="social-number">
-                    <t class="ph-num">Best.Developers</t>
-                    <img src="/img/sk.png">
+                    <t class="ph-num"><a href="callto:Best.Developers">Best.Developers</a></t>
+                    <a href="callto:Best.Developers"><img src="/img/sk.png"></a>
                 </div>
             </div>
         </div>
